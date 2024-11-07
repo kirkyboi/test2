2b4033e3d35676ebea4f91f03deb5ef4235d7380
kirkyboi <136046535+kirkyboi@users.noreply.github.com>
2024-11-07 15:38:36 +0800

    index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MY FIRST UBUTN PROJECT DEPLOYMENT</title>
+    <title>MY FIRST UBUNTU PROJECT DEPLOYMENT</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style1.css">
 </head>
@@ -30,9 +30,9 @@
             </div>
 
             <!-- Age -->
-            <div class="mb-3">
-                <label for="age" class="form-label">Age:</label>
-                <input type="number" class="form-control" id="age" name="age" required>
+           <div class="mb-3">
+                <label for="lastname" class="form-label">Age:</label>
+                <input type="text" class="form-control" id="Age" name="lastname" required>
             </div>
 
             <!-- Address -->
